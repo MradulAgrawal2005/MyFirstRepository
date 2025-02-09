ab4085f79354cb1801c9ee9d4a0fb7f2834ad571
MradulAgrawal2005 <mradulagrawal77@gmail.com>
2025-02-09 23:33:53 +0530

    Add files via upload
    adding my first code on python

---
+++ myfirstfile.py
@@ -0,0 +1,8 @@
+
+# Code to calculate power
+base = int(input("Enter base number: "))
+exponent = int(input("Enter Exponent: "))
+result = base ** exponent
+print(f"{base} raised to the power of {exponent} is: {result}")
+
+
